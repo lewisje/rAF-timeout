e3f9c6d976a46e4aca5edd10ce07fe90881109d5
Joseph Orbegoso Pea <joe@trusktr.io>
2016-03-03 01:45:42 -0800

    Go back to referencing window since code is in strict mode.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 ~function() {
-    const global = this
-
-    const oldSetTimeout = global.setTimeout
-    global.setTimeout = function(fn, timeout) {
-        oldSetTimeout.call(global, () => requestAnimationFrame(fn), timeout)
+    const oldSetTimeout = window.setTimeout
+    window.setTimeout = function(fn, timeout) {
+        oldSetTimeout.call(window, () => requestAnimationFrame(fn), timeout)
     }
 
-    const oldSetInterval = global.setInterval
-    global.setInterval = function(fn, interval) {
-        oldSetInterval.call(global, () => requestAnimationFrame(fn), interval)
+    const oldSetInterval = window.setInterval
+    window.setInterval = function(fn, interval) {
+        oldSetInterval.call(window, () => requestAnimationFrame(fn), interval)
     }
 }()
